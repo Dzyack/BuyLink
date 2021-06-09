0b1fcd97b8975744775c4f39e1de5e3a829c0ac6
Zuhal <72841665+Dzyack@users.noreply.github.com>
2021-06-09 18:08:23 +0700

    Add files via upload

---
+++ Buylink/BuylinkJS/script.js
@@ -0,0 +1,64 @@
+function valid(){
+		swal({
+		  title: "Terima Kasih",
+		  text: "atas kunjungan anda",
+		  icon: "success",
+		  button: "Ok",
+		});
+	}
+	
+let loading = document.getElementById('loading');
+	window.addEventListener('load',function(){
+		loading.style.display="none";
+	});
+	
+document.querySelector("#show-login").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#show-login1").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#show-login2").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#show-login3").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#show-login4").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#show-login5").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#show-login6").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+
